317794d431742263de8b88f38de602ffc3a67c9d
thaabit <mohammedthaabitjacobs@gmail.com>
2021-06-28 10:24:46 +0200

    created global data service project

---
+++ covid-19-global/src/main/java/org/jcode/Covid19GlobalServiceApplication.java
@@ -0,0 +1,13 @@
+package org.jcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19GlobalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19GlobalServiceApplication.class, args);
+	}
+
+}
---
+++ covid-19-global/src/test/java/org/jcode/Covid19GlobalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19GlobalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
